3d5890329df421965e6b74d517d50744d040e899
VGplays <viniciusvvt2005@gmail.com>
2025-03-26 21:46:44 -0300

    Add files via upload

---
+++ vinicius.hs
@@ -0,0 +1,17 @@
+-- questao 1
+--da false a primeira parte, pois ele confere a veracidade de 1 == 4 e(logico) True, mas 1 nao e igual a 3 entao falha o && e retorna False
+--a segunda parte ele faz o (4*8) que da 32, ai faz ele dividido por 31 que da 1.032258064516129, eleva isso por 2 e ai faz - 5, isso da 
+
+-- questao 2
+-- 2**2 da 4.0 e 2^2 da 4
+
+--questao 3
+
+dobro x = x * 2
+
+--questao 4
+
+incremento x = x +1
+decremento x = x-1
+
+--questao 5
